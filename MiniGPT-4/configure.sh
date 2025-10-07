2284057e0007491a1e519f8714aa9ce0d9fe477c
riverfog7 <cjw9770@gmail.com>
2025-10-07 14:59:42 +0900

    change script for linux sed

--- MiniGPT-4/configure.sh
+++ MiniGPT-4/configure.sh
@@ -6,7 +6,7 @@ pushd "${SCRIPT_DIR}/minigpt4" && git reset --hard && popd
 
 mkdir -p weights
 uvx --from 'huggingface_hub[cli]' hf download --repo-type model --local-dir weights/vicuna-13b Vision-CAIR/vicuna --max-workers 16
-sed -i '' "s|please set this value to the path of vicuna model|${SCRIPT_DIR}/weights/vicuna-13b|g" "${SCRIPT_DIR}/minigpt4/minigpt4/configs/models/minigpt4_vicuna0.yaml"
+sed -i "s|please set this value to the path of vicuna model|${SCRIPT_DIR}/weights/vicuna-13b|g" "${SCRIPT_DIR}/minigpt4/minigpt4/configs/models/minigpt4_vicuna0.yaml"
 
 pushd weights
 if [ ! -f pretrained_minigpt4.pth ]; then
@@ -14,14 +14,14 @@ if [ ! -f pretrained_minigpt4.pth ]; then
 fi
 popd
 
-sed -i '' "s|please set this value to the path of pretrained checkpoint|${SCRIPT_DIR}/weights/pretrained_minigpt4.pth|g" "${SCRIPT_DIR}/minigpt4/eval_configs/minigpt4_eval.yaml"
+sed -i "s|please set this value to the path of pretrained checkpoint|${SCRIPT_DIR}/weights/pretrained_minigpt4.pth|g" "${SCRIPT_DIR}/minigpt4/eval_configs/minigpt4_eval.yaml"
 
 #sed -i '' 's/prepare_model_for_int8_training/prepare_model_for_kbit_training/g' "${SCRIPT_DIR}/minigpt4/minigpt4/models/base_model.py"
-sed -i '' '/decord/d' "${SCRIPT_DIR}/minigpt4/minigpt4/datasets/data_utils.py"
+sed -i '/decord/d' "${SCRIPT_DIR}/minigpt4/minigpt4/datasets/data_utils.py"
 
 #sed -i '' "s/'cuda:{}'.format(args.gpu_id)/'mps'/g" "${SCRIPT_DIR}/minigpt4/demo.py"
-sed -i '' "/cudatoolkit/d" "${SCRIPT_DIR}/minigpt4/environment.yml"
-sed -i '' "/decord/d" "${SCRIPT_DIR}/minigpt4/environment.yml"
+sed -i "/cudatoolkit/d" "${SCRIPT_DIR}/minigpt4/environment.yml"
+sed -i "/decord/d" "${SCRIPT_DIR}/minigpt4/environment.yml"
 
 pushd "${SCRIPT_DIR}/minigpt4"
 if ! conda info --envs | grep -q "minigptv"; then
