f05400a2bec762afbe549d8db0a793b35a1b987d
riverfog7 <cjw9770@gmail.com>
2025-10-13 02:00:05 +0900

    Change experiment model type

--- StableDiffusion/configure.sh
+++ StableDiffusion/configure.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
+#MODEL_ID="CompVis/stable-diffusion-v1-4"
+MODEL_ID="stabilityai/stable-diffusion-2-1"
 PREDOWNLOAD_MODEL=1
 export HF_HOME="$(cd $(dirname $0)/.. ; pwd)/.hf_home"
 
@@ -8,5 +10,5 @@ cd "${SCRIPT_DIR}"
 
 if [ "$PREDOWNLOAD_MODEL" -eq 1 ]; then
   mkdir -p "${HF_HOME}"
-  uvx --from 'huggingface_hub[cli]' --with 'hf_transfer' hf download --repo-type model --cache-dir "${HF_HOME}" CompVis/stable-diffusion-v1-4 --max-workers 16
+  uvx --from 'huggingface_hub[cli]' --with 'hf_transfer' hf download --repo-type model --cache-dir "${HF_HOME}" "${MODEL_ID}" --max-workers 16
 fi
